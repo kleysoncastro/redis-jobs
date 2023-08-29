0d715131a5252305daf96add825bdb49d9cf90ad
kleyson Castro <kleysoncastro99@gmail.com>
2023-08-29 08:14:26 -0300

    chore: patter singleton para conexao redis

---
+++ src/app/lib/Redis.ts
@@ -0,0 +1,69 @@
+import Redis from "redis";
+
+class RedisManager {
+  client;
+  constructor() {
+    this.client = Redis.createClient({
+      host: "localhost",
+      port: 6379,
+    });
+
+    this.client.on("error", (err) => {
+      console.error("Erro na conexão Redis:", err);
+    });
+
+    this.client.on("end", () => {
+      console.log("Conexão Redis encerrada.");
+    });
+
+    // Encerre a conexão ao encerrar o processo
+    process.on("exit", () => {
+      this.close();
+    });
+  }
+  /**
+   * Exclui uma chave no Redis e retorna um código indicando o resultado.
+   *
+   * @param {string} key A chave que será excluída.
+   * @returns {Promise<number>} Um código indicando o resultado da exclusão:
+   *   - 0: Chave excluída com sucesso.
+   *   - 1: Erro ao excluir a chave.
+   *   - 2: Chave não encontrada.
+   *   - -1: Erro geral.
+   */
+  async delKey(key: string): Promise<number> {
+    try {
+      const result = await new Promise<number>((resolve, reject) => {
+        this.client.del(key, (err, result) => {
+          if (err) {
+            console.error("Erro ao excluir chave:", err);
+            reject(1);
+          } else {
+            if (result === 1) {
+              console.log(`Chave "${key}" excluída com sucesso.`);
+              this.close();
+              resolve(0);
+            } else {
+              console.log(`Chave "${key}" não encontrada.`);
+              resolve(2);
+            }
+          }
+        });
+      });
+
+      return result;
+    } catch (error) {
+      console.error("Erro ao executar delKey:", error);
+      return -1; // Ou qualquer valor que indique erro
+    }
+  }
+
+  close() {
+    this.client.quit();
+    console.log("Conexão Redis fechada.");
+  }
+}
+
+const clientRedis = new RedisManager();
+
+export default clientRedis;
